fc4bb4dbeafe676cfd1d1a5dc975616181d50a39
Dragan Stanisavljevic <Dragan.Stanisavljevic@tomtom.com>
2016-03-30 17:45:03 +0300

    Added new project

---
+++ tests/pages/filterButtonPanel.js
@@ -0,0 +1,12 @@
+this.FilterButtonsPanel = function (driver) {
+  GalenPages.extendPage(this, driver, "Filter Buttons Panel", {
+
+    popularityBut:  "//*[@id='filter-sort-buttons']/label[1]", // xpath locator
+	newBut:			"//*[@id='filter-sort-buttons']/label[2]",
+	
+	hoverNewButton: function () {
+      this.newBut.hover();
+    }
+
+  });
+};
