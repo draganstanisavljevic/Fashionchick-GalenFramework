c31209184c52d461095ae5971f3a8f118ff80dc6
Dragan Stanisavljevic <Dragan.Stanisavljevic@tomtom.com>
2016-03-31 16:56:01 +0300

    Formatted file

--- tests/pages/filterButtonPanel.js
+++ tests/pages/filterButtonPanel.js
@@ -1,16 +1,16 @@
 this.FilterButtonsPanel = function (driver) {
-  GalenPages.extendPage(this, driver, "Filter Buttons Panel", {
+    GalenPages.extendPage(this, driver, "Filter Buttons Panel", {
 
-    popularityBut:  "//*[@id='filter-sort-buttons']/label[1]", // xpath locator
-	newBut:			"//*[@id='filter-sort-buttons']/label[2]",
+        popularityBut:    "//*[@id='filter-sort-buttons']/label[1]", // xpath locator
+        newBut:           "//*[@id='filter-sort-buttons']/label[2]", // xpath locator
 	
-	hoverNewButton: function () {
-      this.newBut.hover();
-    },
+        hoverNewButton: function () {
+          this.newBut.hover();
+        },
 	
-	clickNewButton: function () {
-      this.newBut.click();
-    }	
+        clickNewButton: function () {
+          this.newBut.click();
+        }	
 
-  });
+    });
 };
